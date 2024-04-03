4b90a77bc55a6d7adf9961a0ed583df593bae08f
Sai Karthik Reddy <ssaikarthik635@gmail.com>
2024-04-03 15:52:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import "./App.css";
+
+function App() {
+  return (
+    <div>
+      <button className="toggle-btn">Sidebar</button>
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Services</li>
+        <li>Contact</li>
+      </ul>
+    </div>
+  );
+}
+
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
+
+export default App;
